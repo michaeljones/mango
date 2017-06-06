96282ca43646c6644584623cc0d662a5d1317916
Michael Jones <m.pricejones@gmail.com>
2017-06-06 21:06:52 +0100

    Switch to Options for inputs and add yaml

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 
+extern crate yaml_rust;
+use yaml_rust::YamlLoader;
+use std::fs::File;
+use std::io::prelude::*;
+
 trait Node {
     fn pull(&self) -> Vec<String>;
 }
@@ -15,32 +20,42 @@ impl Node for StandardIn {
 
 
 struct StandardOut<'a> {
-    input: &'a Node,
+    input: Option<&'a Node>,
 }
 
 impl<'a> Node for StandardOut<'a> {
     fn pull(&self) -> Vec<String> {
-        let content = self.input.pull();
-        println!("{:?}", content);
-        return vec![];
+        match self.input {
+            None => return vec![],
+            Some(input) => {
+                let content = input.pull();
+                println!("{:?}", content);
+                return vec![];
+            }
+        }
     }
 }
 
 struct StringMatch<'a> {
-    input: &'a Node,
+    input: Option<&'a Node>,
     value: String,
 }
 
 impl<'a> Node for StringMatch<'a> {
     fn pull(&self) -> Vec<String> {
-        let content = self.input.pull();
-        let mut output = vec![];
-        for i in &content {
-            if i.contains(self.value.as_str()) {
-                output.push(i.to_string());
+        match self.input {
+            None => return vec![],
+            Some(input) => {
+                let content = input.pull();
+                let mut output = vec![];
+                for i in &content {
+                    if i.contains(self.value.as_str()) {
+                        output.push(i.to_string());
+                    }
+                }
+                return output;
             }
         }
-        return output;
     }
 }
 
@@ -49,12 +64,19 @@ fn pull<T: Node>(node: &T) -> Vec<String> {
 }
 
 fn main() {
+
+    let mut file = File::open("example.yaml").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let docs = YamlLoader::load_from_str(contents.as_str()).unwrap();
+
     let input = StandardIn {};
-    let stringMatch = StringMatch {
-        input: &input,
+    let string_match = StringMatch {
+        input: Some(&input),
         value: "abc".to_string(),
     };
-    let output = StandardOut { input: &stringMatch };
+    let output = StandardOut { input: Some(&string_match) };
 
     // connect(input, stringMatch);
     // connect(stringMatch, output);
