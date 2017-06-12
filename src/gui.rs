54287d184813f8ecc6928fe08b3831e28faf4b57
Michael Jones <m.pricejones@gmail.com>
2017-06-12 19:24:04 +0100

    Custom widget type
    For drawing a node. Very much hoping it'll give me access to drag & drop
    events in the 'update' method which were otherwise unavailable outside
    of it.

--- src/gui.rs
+++ src/gui.rs
@@ -5,8 +5,191 @@
 //! `EventLoop` in the `examples/support` module. It has been inlined in order to provide an
 //! example that does not depend on the `support` module.
 
-
 pub mod feature {
+    pub mod gui_node {
+        use conrod::{self, widget, Colorable, Dimensions, Labelable, Point, Positionable, Widget};
+
+        pub struct GuiNode {
+            common: widget::CommonBuilder,
+            label: String,
+            x: f64,
+            y: f64,
+            style: Style,
+            enabled: bool,
+        }
+
+        // We use the `widget_style!` macro to vastly simplify the definition and implementation of the
+        // widget's associated `Style` type. This generates both a `Style` struct, as well as an
+        // implementation that automatically retrieves defaults from the provided theme.
+        //
+        // See the documenation of the macro for a more details.
+        widget_style! {
+            style Style {
+                - color: conrod::Color { theme.shape_color }
+                - label_color: conrod::Color { theme.label_color }
+                - label_font_size: conrod::FontSize { theme.font_size_medium }
+                - label_font_id: Option<conrod::text::font::Id> { theme.font_id }
+            }
+        }
+
+        // We'll create the widget using a `Circle` widget and a `Text` widget for its label.
+        //
+        // Here is where we generate the type that will produce these identifiers.
+        widget_ids! {
+            struct Ids {
+                node,
+                text,
+            }
+        }
+
+        pub struct State {
+            ids: Ids,
+        }
+
+        pub fn is_over_circ(circ_center: Point, mouse_point: Point, dim: Dimensions) -> bool {
+            // Offset vector from the center of the circle to the mouse.
+            let offset = conrod::utils::vec2_sub(mouse_point, circ_center);
+
+            // If the length of the offset vector is less than or equal to the circle's
+            // radius, then the mouse is inside the circle. We assume that dim is a square
+            // bounding box around the circle, thus 2 * radius == dim[0] == dim[1].
+            let distance = (offset[0].powf(2.0) + offset[1].powf(2.0)).sqrt();
+            let radius = dim[0] / 2.0;
+            distance <= radius
+        }
+
+        impl GuiNode {
+            pub fn new(label: String, x: f64, y: f64) -> Self {
+                GuiNode {
+                    common: widget::CommonBuilder::new(),
+                    label: label,
+                    x: x,
+                    y: y,
+                    style: Style::new(),
+                    enabled: true,
+                }
+            }
+
+            pub fn label_font_id(mut self, font_id: conrod::text::font::Id) -> Self {
+                self.style.label_font_id = Some(Some(font_id));
+                self
+            }
+        }
+
+        /// A custom Conrod widget must implement the Widget trait. See the **Widget** trait
+        /// documentation for more details.
+        impl Widget for GuiNode {
+            /// The State struct that we defined above.
+            type State = State;
+            /// The Style struct that we defined using the `widget_style!` macro.
+            type Style = Style;
+            /// The event produced by instantiating the widget.
+            ///
+            /// `Some` when clicked, otherwise `None`.
+            type Event = Option<()>;
+
+            fn common(&self) -> &widget::CommonBuilder {
+                &self.common
+            }
+
+            fn common_mut(&mut self) -> &mut widget::CommonBuilder {
+                &mut self.common
+            }
+
+            fn init_state(&self, id_gen: widget::id::Generator) -> Self::State {
+                State { ids: Ids::new(id_gen) }
+            }
+
+            fn style(&self) -> Self::Style {
+                self.style.clone()
+            }
+
+            /// Update the state of the button by handling any input that has occurred since the last
+            /// update.
+            fn update(self, args: widget::UpdateArgs<Self>) -> Self::Event {
+                use conrod::{color, Sizeable};
+
+                let widget::UpdateArgs {
+                    id,
+                    maybe_parent_id,
+                    state,
+                    rect,
+                    mut ui,
+                    style,
+                    ..
+                } = args;
+
+                let (color, event) = {
+                    let input = ui.widget_input(id);
+
+                    // If the button was clicked, produce `Some` event.
+                    let event = input.clicks().left().next().map(|_| ());
+
+                    let color = style.color(&ui.theme);
+                    let color = input
+                        .mouse()
+                        .map_or(color,
+                                |mouse| if is_over_circ([0.0, 0.0], mouse.rel_xy(), rect.dim()) {
+                                    if mouse.buttons.left().is_down() {
+                                        color.clicked()
+                                    } else {
+                                        color.highlighted()
+                                    }
+                                } else {
+                                    color
+                                });
+
+                    (color, event)
+                };
+
+                let parent_id = maybe_parent_id.unwrap_or(id);
+
+                // Draw the node
+                widget::Canvas::new()
+                    .graphics_for(id)
+                    .parent(parent_id)
+                    .top_left_with_margins_on(parent_id, self.y, self.x)
+                    .w(100.0)
+                    .h(30.0)
+                    .color(color::BLACK)
+                    .set(state.ids.node, ui);
+
+                widget::primitive::text::Text::new(self.label.as_str())
+                    .parent(state.ids.node)
+                    .color(color::WHITE)
+                    .middle_of(state.ids.node)
+                    .set(state.ids.text, ui);
+
+                event
+            }
+        }
+
+        /// Provide the chainable color() configuration method.
+        impl Colorable for GuiNode {
+            fn color(mut self, color: conrod::Color) -> Self {
+                self.style.color = Some(color);
+                self
+            }
+        }
+
+        /// Provide the chainable label(), label_color(), and label_font_size()
+        /// configuration methods.
+        impl<'a> Labelable<'a> for GuiNode {
+            fn label(mut self, text: &'a str) -> Self {
+                self.label = text.to_string();
+                self
+            }
+            fn label_color(mut self, color: conrod::Color) -> Self {
+                self.style.label_color = Some(color);
+                self
+            }
+            fn label_font_size(mut self, size: conrod::FontSize) -> Self {
+                self.style.label_font_size = Some(size);
+                self
+            }
+        }
+    }
+
     use conrod;
     use conrod::backend::glium::glium;
     use conrod::backend::glium::glium::glutin;
@@ -27,7 +210,6 @@ pub mod feature {
 
     struct GuiNode {
         pub id: conrod::widget::id::Id,
-        pub text_id: conrod::widget::id::Id,
         pub name: String,
         pub x: i32,
         pub y: i32,
@@ -115,10 +297,8 @@ pub mod feature {
 
                 // Use the `winit` backend feature to convert the winit event to a conrod one.
                 if let Some(event) = conrod::backend::winit::convert(event.clone(), &display) {
-                    println!("{:?}", &event);
                     ui.handle_event(event);
                     ui_needs_update = true;
-
                 }
 
                 match event {
@@ -189,21 +369,11 @@ pub mod feature {
             }
         }
 
-        for gui_node in params.gui_nodes.iter() {
-            let node = gui_node.borrow_mut();
-            widget::Canvas::new()
+        for g_node in params.gui_nodes.iter() {
+            let node = g_node.borrow_mut();
+            gui_node::GuiNode::new(node.name.clone(), node.x as f64, node.y as f64)
                 .parent(ids.canvas)
-                .color(color::BLACK)
-                .pad(5.0)
-                .w(100.0)
-                .h(30.0)
-                .top_left_with_margins_on(ids.canvas, node.y as f64, node.x as f64)
                 .set(node.id, ui);
-
-            widget::primitive::text::Text::new(node.name.as_str())
-                .color(color::WHITE)
-                .top_left_of(node.id)
-                .set(node.text_id, ui);
         }
 
         widget::Scrollbar::y_axis(ids.canvas)
@@ -212,14 +382,13 @@ pub mod feature {
     }
 
     fn create_node(params: &mut Params, mut generator: conrod::widget::id::Generator) {
-        let gui_node = Rc::new(RefCell::new(GuiNode {
-                                                id: generator.next(),
-                                                text_id: generator.next(),
-                                                name: params.name_input.clone(),
-                                                x: params.tab_x,
-                                                y: params.tab_y,
-                                            }));
+        let g_node = Rc::new(RefCell::new(GuiNode {
+                                              id: generator.next(),
+                                              name: params.name_input.clone(),
+                                              x: params.tab_x,
+                                              y: params.tab_y,
+                                          }));
 
-        params.gui_nodes.push(gui_node);
+        params.gui_nodes.push(g_node);
     }
 }
